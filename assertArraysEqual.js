55861549b558072254e1d41eba9c72e56464657f
Gary Chen <garychen.whc@gmail.com>
2019-08-20 16:20:41 +0000

    revised for array = empty

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,21 @@
-const eqArrays = function (arr1, arr2) {
+const assertArraysEqual = function (arr1, arr2) {
+
+  /* check if the length is the same */
   if (arr1.length !== arr2.length) {
     console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
   } else {
+
+    /* if array is empty */
+    if (arr1.length === 0 && arr1.length === 0) {
+      console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`)
+    }
+
+    /* if array is not empty */
     for (let i = 0; i < arr1.length; ++i) {
       if (arr1[i] !== arr2[i]) {
         console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-        break;  
+        break;
       }
-
       if (i === arr1.length - 1) {
         console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
       }
@@ -15,8 +23,10 @@ const eqArrays = function (arr1, arr2) {
   }
 }
 
-// eqArrays([1, 2, 3], [1, 2, 3]);
-// eqArrays([1, 2, 3], [3, 2, 1]);
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-// eqArrays(["1", "2", "3"], ["1", "2", 3]);
-// eqArrays([1, 2, 3], [1, 2, 3]);
+
+assertArraysEqual([], [])
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [3, 2, 1]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
