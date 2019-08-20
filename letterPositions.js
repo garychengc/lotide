70fa468e242b6470986b51186b66bd609c2f61f3
Gary Chen <garychen.whc@gmail.com>
2019-08-20 21:01:33 +0000

    letterPosition exercise added

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length != arr2.length) return false;
+  for (let i = 0; i < arr1.length; ++i) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; ++i) {
+    if (sentence[i] !== ' '){
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }    
+  return results;
+};
+
+// console.log(letterPositions("lighthouse in the house"));
+// assertArraysEqual(letterPositions("lighthouse in the house")['i'], [1, 11]);
