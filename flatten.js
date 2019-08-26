5590c074dcff77fdc9d6a5aea395da8be9f97a92
Gary Chen <garychen.whc@gmail.com>
2019-08-26 15:34:09 +0000

    flatten refactored

--- flatten.js
+++ flatten.js
@@ -1,30 +1,14 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; ++i) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} === ${arr2}`);
-  }
-};
-
 const flatten = function(array) {
   let flattenedArray = [];
-  array.map(num => {
+
+  array.forEach(num => {
     if (!Array.isArray(num)) {
-      flattenedArray.push(num);
+      return flattenedArray.push(num);
     } else {
-      flattenedArray = flattenedArray.concat(num);
+      return flattenedArray = flattenedArray.concat(flatten(num));
     }
   });
   return flattenedArray;
 };
+module.exports = flatten;
 
-// console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]));
-// console.log(flatten([1, [2, [3, [4]], 5]]));
