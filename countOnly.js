5abe47ed4501896eac173ffcdf5d6b6b626ac780
Gary Chen <garychen.whc@gmail.com>
2019-08-26 17:33:37 +0000

    countOnly refactored

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,3 @@
-const assertEqual = function(actual, expected) {
-  return (actual === expected) ? `✅ Assertion Passed: ${actual} === ${expected}` : `❌ Assertion Failed: ${actual} !== ${expected}`
-};
-
 const countOnly = function(allItems, itemsToCount) {
   let nameObj = {};
   allItems.forEach(name => {
@@ -15,21 +11,4 @@ const countOnly = function(allItems, itemsToCount) {
   });
   return nameObj;
 }
-
-
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-
-// const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true });
-// console.log(assertEqual(result1["Jason"], 1));
-// console.log(assertEqual(result1["Karima"], undefined));
-// console.log(assertEqual(result1["Fang"], 2));
+module.exports = countOnly;
