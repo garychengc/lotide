0ff8d9500805b0c9442375a228347f621bd27cdb
Gary Chen <garychen.whc@gmail.com>
2019-08-26 16:21:58 +0000

    findKey refactored

--- findKey.js
+++ findKey.js
@@ -1,26 +1,8 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     return (`✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     return (`❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 function findKey(obj, callback) {
   for (let key in obj) {
     if (callback(obj[key])) {
       return key;
     }
   }
-}
-
-
-// console.log(assertEqual(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), 'noma'));
-
+};
+module.exports = findKey;
