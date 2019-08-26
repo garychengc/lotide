b8ef7f2510f356285da45303c50ea931b555b26b
Gary Chen <garychen.whc@gmail.com>
2019-08-26 15:48:50 +0000

    findKeyByValue refactored

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,25 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return (`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const findKeyByValue = function (obj, value) {
+const findKeyByValue = function(obj, value) {
   for (let key in obj) {
     if (obj[key] === value) {
       return key;
     }
   }
-}
-
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
 };
-
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
-console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
+module.exports = findKeyByValue;
