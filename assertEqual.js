09f01c87e34cfc7a827e8918cdfb6b9059032e82
Gary Chen <garychen.whc@gmail.com>
2019-08-26 02:19:48 +0000

    from wet to dry method

--- assertEqual.js
+++ assertEqual.js
@@ -5,12 +5,7 @@ const assertEqual = function(actual, expected) {
     return (`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+module.exports = assertEqual;
 
-// // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual('I am cool', 'I am cool');
-// assertEqual('I am cool', 'I am very cool');
-// assertEqual(1, 2);
 
 
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual('I am cool', 'I am cool'));
+console.log(assertEqual('I am cool', 'I am very cool'));
+console.log(assertEqual(1, 2));
