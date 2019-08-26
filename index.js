710c8b1745dee7bed042f48f105b54faaf59f0d2
Gary Chen <garychen.whc@gmail.com>
2019-08-26 15:36:45 +0000

    flatten added

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const letterPosition = require('./letterPositions');
+const flatten = require('./flatten');
+
+
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  without: without,
+  takeUntil: takeUntil,
+  map: map,
+  letterPosition: letterPosition,
+  flatten: flatten,
+  
+
 };
